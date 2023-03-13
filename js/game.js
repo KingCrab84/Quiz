86a47ed0ddd879bdb125ef4223fff41eff2bf8f4
Cuong Lien <Kunglien1984@hotmail.com>
2023-03-13 11:54:43 +0000

    game js creating array

---
+++ js/game.js
@@ -0,0 +1,12 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
