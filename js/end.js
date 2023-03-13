94683981708dcad057b7cecd4054e59273af2687
Cuong Lien <Kunglien1984@hotmail.com>
2023-03-13 15:19:51 +0000

    end js most recent score

--- js/end.js
+++ js/end.js
@@ -1,11 +1,13 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
 const mostRecentScore = window.localStorage["mostRecentScore"];
 
+const MAX_HIGH_SCORES = 5;
+finalScore.innerText = `Score: ${mostRecentScore}`;
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value; 
 });
-
     
 saveScoreBtn.addEventListener("click", e => {
     e.preventDefault();
