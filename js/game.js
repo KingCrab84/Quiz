6da8c3bbe4bc3f5a8f040ed1441395df7b37f966
Cuong Lien <Kunglien1984@hotmail.com>
2023-03-13 13:11:15 +0000

    game js progressbar

--- js/game.js
+++ js/game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById("progressText")
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull')
 
 let currentQuestion = {};
 let acceptingAnswers = false;
